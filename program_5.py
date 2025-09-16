c2783fd6ff0378a589153ed4b3987538d0a355c0
boyofdoom09-maker <boyofdoom09@outlook.com>
2025-09-16 15:05:02 -0500

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -4,3 +4,19 @@
 # Also tax is 7%. 
 # Write a program the inputs the type of hot dog wanted and optional toppings.  
 # The program then displays the hot dog cost, tax and total cost. 
+
+taxrate = 0.07  
+hotdog_price = 3.50
+chilidog_price = 4.50
+
+hotdogtype = input('What kind of hot dog would you like:')
+if hotdogtype == hotdog or hot dog or Hotdog or Hotdog 
+    hotdog_price = float(3.50) 
+elif hotdogtype == chilidog or chili dog or Chilidog or Chilidog 
+    hotdog_price = float(4.50) 
+cheese = float(0.50)
+peppers = float(0.75)
+grilledonions = float(1.00)
+
+
+
